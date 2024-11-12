2583959f8136aadb4840217ad3e22e3444a66e15
Olesia <lisa999lisa@mail.ru>
2024-11-12 22:16:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/dk5gs1t3dfbs7cje/branch/main?svg=true)](https://ci.appveyor.com/project/Ollessia/dz-1-2api-ci/branch/main)
---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData(){
+    // Given - When - Then
+// Предусловия
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("HELLO")
+  .when().log().all()
+  .post("/post")
+  .then().log().all()
+  .statusCode(200)
+  .body("data", org.hamcrest.Matchers.equalTo("HI"));
+
+    }
+
+}
